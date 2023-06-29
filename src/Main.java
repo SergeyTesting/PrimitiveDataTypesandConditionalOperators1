9f8c3e832b91dcaa5fc5e0c652802b5dc30544a1
Sergey Filatov <sergey_filatov_1991@mail.ru>
2023-06-29 18:51:21 +0300

    Gitignore added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         int price = 12_600; //стоимость билета
         int bonusPerMile = 20; //колличество рублей для одной бонусной мили
         int bonusMiles = price / bonusPerMile; //количество бонусных миль
